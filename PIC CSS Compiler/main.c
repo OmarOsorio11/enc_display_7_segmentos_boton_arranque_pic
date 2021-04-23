615b4709b461e644a76e6eca53aceb212645263c
Omar Osorio <triggeruseromar@gmail.com>
2021-04-23 12:16:48 -0500

    first commit

---
+++ PIC CSS Compiler/main.c
@@ -0,0 +1,32 @@
+#include <16F1827.h>
+#device ADC=16
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES NOBROWNOUT               //No brownout reset
+#use delay(crystal=4MHz)
+#use standard_io(b) 
+#use standard_io(a) 
+void main()
+{
+   set_tris_b(0b00000000);
+   set_tris_a(0b11111111);
+   
+   int numeros[10]={0x3F,0x6,0x5B,0x4F,0x66,0x6D,0x7D,0x7,0x7F,0x6F};
+   int contar = 0;
+   output_b(0x3F);
+   while(TRUE)
+   {
+      if(input_state(PIN_A0))
+      {   
+         for (contar=0;contar<10;contar++)
+         {
+            output_b(numeros[contar]);
+            delay_ms(300);            
+         }
+      }
+      else {
+         output_b(0x3F);
+      }
+   }
+}
+
+
---
+++ PIC CSS Compiler/main.h
@@ -0,0 +1,3 @@
+#include <16f887.h>  //Libreria del PIC
+#fuses XT, NOWDT, NOPROTECT,NOPUT   //Fuses del PIC
+#use delay(clock=4000000) //Especifica la velocidad de reloj
